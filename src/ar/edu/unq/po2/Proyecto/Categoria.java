776dc64cb6c8c72e1d75d0c791e21306372e2276
Federico <Federico@Federico>
2022-10-22 15:53:59 -0300

    - Se crearon paquetes referentes a Buscador de proyectos, Usuarios, Desafios, y Proyecto. - Se añadieron algunas clases referidas a esos paquetes.

---
+++ src/ar/edu/unq/po2/Proyecto/Categoria.java
@@ -0,0 +1,7 @@
+package ar.edu.unq.po2.Proyecto;
+
+public enum Categoria {
+	
+	FÍSICA, QUÍMICA, ASTRONOMÍA, BIOLOGÍA, ZOOLOGÍA, BOTÁNICA, GEOLOGÍA, METEOROLOGÍA
+	
+}
---
+++ src/ar/edu/unq/po2/SistemaUsuario/Gusto.java
@@ -0,0 +1,5 @@
+package ar.edu.unq.po2.SistemaUsuario;
+
+public enum Gusto {			// Modela los gustos del usuario
+
+}
