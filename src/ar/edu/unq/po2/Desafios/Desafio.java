d47248a64949f81064c797e51fd7c1ba5139dadf
Ivan Gomez <ivangzgomez@gmail.com>
2022-10-26 23:38:21 -0300

    desafio y estados

--- src/ar/edu/unq/po2/Desafios/Desafio.java
+++ src/ar/edu/unq/po2/Desafios/Desafio.java
@@ -15,6 +15,50 @@ public class Desafio implements ElementoDeLudificacion {
 	//private ArrayList<Actividad> actividades;
 	private int porcentaje;
 	private Date fechaCompletado;
+	private int desafiosAceptados;
+	private int voto;
+	
+	
+	public ArrayList<Date> getRestriccionTemporal() {
+		return restriccionTemporal;
+	}
+
+	public void setRestriccionTemporal(ArrayList<Date> restriccionTemporal) {
+		this.restriccionTemporal = restriccionTemporal;
+	}
+
+	public int getCantidadObjetivoDeMuestras() {
+		return cantidadObjetivoDeMuestras;
+	}
+
+	public void setCantidadObjetivoDeMuestras(int cantidadObjetivoDeMuestras) {
+		this.cantidadObjetivoDeMuestras = cantidadObjetivoDeMuestras;
+	}
+
+	public Dificultad getDificultad() {
+		return dificultad;
+	}
+
+	public void setDificultad(Dificultad dificultad) {
+		this.dificultad = dificultad;
+	}
+
+	public double getRecompensa() {
+		return recompensa;
+	}
+
+	public void setRecompensa(double recompensa) {
+		this.recompensa = recompensa;
+	}
+
+	public int getPorcentaje() {
+		return porcentaje;
+	}
+
+	public void setPorcentaje(int porcentaje) {
+		this.porcentaje = porcentaje;
+	}
+
 	private Date fechaAceptado;
 	private EstadoDesafio estado;
 	
@@ -38,8 +82,31 @@ public class Desafio implements ElementoDeLudificacion {
 		return estado;
 	}
 
+	public int getVoto() {
+		return voto;
+	}
+
 	public void setEstado(EstadoDesafio estado) {
 		this.estado = estado;
 	}
+
+	public int getDesafiosAceptados() {
+		return desafiosAceptados;
+	}
+
+	public void AceptarDesafio() {
+		this.desafiosAceptados = desafiosAceptados + 1 ;  
+	}
+	
+	public void votar(int n ) {
+		if(n >= 0 && n <= 5 ) {
+			this.voto = n;
+		}
+	}
+	
+	
+	
+	
+	
 
 }
--- src/ar/edu/unq/po2/EstadoDesafio/EstadoDesafio.java
+++ src/ar/edu/unq/po2/EstadoDesafio/EstadoDesafio.java
@@ -1,7 +1,9 @@
 package ar.edu.unq.po2.EstadoDesafio;
 
 public interface EstadoDesafio {
-
+	
+//	private EstadoDesafio estado ; // Deberia empezar en NoAceptado 
+	
 	public void aceptar();
 	public void completar();
 	
