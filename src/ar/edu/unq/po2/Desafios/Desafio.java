0c86f4bae0034ca59d94d5f0c2754c6c2bf16a8b
Ivan Gomez <ivangzgomez@gmail.com>
2022-10-26 21:49:53 -0300

    comentando errores

--- src/ar/edu/unq/po2/Desafios/Desafio.java
+++ src/ar/edu/unq/po2/Desafios/Desafio.java
@@ -2,14 +2,14 @@ package ar.edu.unq.po2.Desafios;
 
 public class Desafio implements ElementoDeLudificacion {
 	
-	private AreaGeografica area;
-	private ArrayList<Date> restriccionTemporal;
-	private int cantidadObjetivoDeMuestras;
-	private Dificultad dificultad;
-	private double recompensa;
-	private ArrayList<Actividad> actividades;
-	private int porcentaje;
-	private Date fechaCompletado;
-	private Date fechaAceptado;
+//	private AreaGeografica area;
+//	private ArrayList<Date> restriccionTemporal;
+//	private int cantidadObjetivoDeMuestras;
+//	private Dificultad dificultad;
+//	private double recompensa;
+//	private ArrayList<Actividad> actividades;
+//	private int porcentaje;
+//	private Date fechaCompletado;
+//	private Date fechaAceptado;
 	
 }
