dd5d67143066955b31fe88d821a0ea371fa90f77
ZekielAsh <sherlock-ezequiel@hotmail.com>
2022-11-03 16:07:56 -0300

    Agregadas clases AreaGeografica y Ubicacion.
    Problema presentado: no me saltan los errores en eclipse a pesar de que
    existen, y no veo nada como un paquete

---
+++ src/ar/edu/unq/po2/Proyecto/AreaGeografica.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import organizacion.ObservadorZona;
+import organizacion.ZonaDeCobertura;
+import ubicacion.Ubicacion;
+
+public class AreaGeografica {
+	private String nombre;
+	private Ubicacion epicentro;
+	private float radio;
+	
+	public AreaGeografica(String nombre, float latitud, float longitud, float radio) {
+		this.setNombre(nombre);
+		this.setEpicentro(latitud, longitud);
+		this.setRadio(radio);
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public Ubicacion getEpicentro() {
+		return epicentro;
+	}
+	public void setEpicentro(float latitud, float longitud) {
+		this.epicentro = new Ubicacion(latitud, longitud);
+	}
+		public float getRadio() {
+		return radio;
+	}
+	public void setRadio(float radio) {
+		this.radio = radio;
+	}
+	
+	public boolean seSolapan(AreaGeografica area) {
+		return (this.radio + area.radio >= 
+				this.getEpicentro().distancia(area.getEpicentro()));
+	}
+}
