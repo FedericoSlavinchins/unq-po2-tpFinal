c82720c9c17a5dc93393c686c37cef5660b47d7f
Ivan Gomez <ivangzgomez@gmail.com>
2022-11-05 20:35:50 -0300

    Agregando votacion

--- src/ar/edu/unq/po2/SistemaUsuario/Usuario.java
+++ src/ar/edu/unq/po2/SistemaUsuario/Usuario.java
@@ -136,7 +136,10 @@ public class Usuario {
 	public ArrayList<DesafioDeUsuario> getDesafiosCompletados() {
 		return desafiosCompletados;
 	}
-	
+		
+	public void votar(DesafioDeUsuario desafio,int valorDeVoto) {
+		desafio.setVoto(valorDeVoto);
+	}
 	
 	
 	
