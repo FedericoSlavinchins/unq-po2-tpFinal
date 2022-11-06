2e7554970cf19baa0af5874586880fa54790f464
FedericoSlavinchins <FedericoSlavinchins@gmail.com>
2022-11-06 17:12:26 -0300

    Merge branch 'main-2' of https://github.com/FedericoSlavinchins/unq-po2-tpFinal.git into main-2

--- test/ar/edu/unq/po2/SistemaUsuario/UsuarioTest.java
+++ test/ar/edu/unq/po2/SistemaUsuario/UsuarioTest.java
@@ -10,35 +10,39 @@ import org.junit.jupiter.api.Test;
 import ar.edu.unq.po2.Desafios.Desafio;
 import ar.edu.unq.po2.Desafios.DesafioDeUsuario;
 import ar.edu.unq.po2.Proyecto.Categoria;
-import ar.edu.unq.po2.Proyecto.Geocoordenada;
 import ar.edu.unq.po2.Proyecto.Muestra;
 import ar.edu.unq.po2.Proyecto.Proyecto;
 import ar.edu.unq.po2.Proyecto.Ubicacion;
 
-import static org.mockito.Mockito.*;
 
 class UsuarioTest {
 	
 	private Usuario usuario;
 	private DesafioDeUsuario desafioDeUsuario;
+	private DesafioDeUsuario desafioDeUsuario2;
 	private Desafio desafio;
+	private Desafio desafio2;
 	private Proyecto proyecto;
 	private ArrayList<Categoria> listaDeCategorias;
 	private Categoria categoria;
 	private Muestra muestra;
+	private Muestra muestra2;
 	private Ubicacion geocoordenada;
 	
 	@BeforeEach
 	public void setUp() {
 		desafio = new Desafio();
+		desafio2 = new Desafio();
 		desafioDeUsuario = new DesafioDeUsuario(desafio);
+		desafioDeUsuario2 = new DesafioDeUsuario(desafio2);
 		usuario = new Usuario("nombreUsuario");
 		geocoordenada = new Ubicacion(0, 0);
 		muestra = new Muestra(usuario, geocoordenada);
+		muestra2 = new Muestra(usuario, geocoordenada);
 		listaDeCategorias = new ArrayList<Categoria>();
 		categoria = new Categoria("Biología");
 		listaDeCategorias.add(categoria);
-		proyecto = mock(Proyecto.class);
+		proyecto = new Proyecto("proyecto", "descripcion", listaDeCategorias);
 	}
 
 	@Test
@@ -47,11 +51,6 @@ class UsuarioTest {
 		assertTrue(usuario.getProyectos().contains(proyecto));
 	}
 	
-	@Test
-	void cuandoElUsuarioRecolectaUnaMuestraSeGuardaEnSuVariableMuestras() {
-		usuario.recolectarMuestra(muestra, proyecto);
-		assertTrue(usuario.getMuestrasSubidas().contains(muestra));
-	}
 	
 	@Test
 	void cuandoElUsuarioAceptaUnDesafioDeUsuarioElMismoSeGuardaEnSuListaDeDesafiosAceptados() {
@@ -62,9 +61,77 @@ class UsuarioTest {
 	
 	@Test
 	void cuandoElUsuarioCompletaUnDesafioDeUsuarioElMismoSeGuardaEnSuListaDeDesafiosCompletadosYDesapareceDeAceptados() {
-		usuario.completarDesafioDeUsuario(desafioDeUsuario);
+		usuario.completarDesafioDeUsuario(desafioDeUsuario, 5);
 		assertTrue(usuario.getDesafiosCompletados().contains(desafioDeUsuario));
 		assertFalse(usuario.getDesafiosAceptados().contains(desafioDeUsuario));
 	}
-
+	
+	@Test
+	void cuandoElUsuarioCompletaUnDesafioDeUsuarioElMismoVotaAlUsuario() {
+		usuario.completarDesafioDeUsuario(desafioDeUsuario, 5);
+		boolean resultadoActual = desafioDeUsuario.getVoto() == 5;
+		assertTrue(resultadoActual);
+	}
+	
+	@Test
+	void cuandoElUsuarioRecolectaUnaMuestraLaMismaFormaParteDelProyecto() {
+		usuario.recolectarMuestra(muestra, proyecto);
+		assertTrue(proyecto.getMuestras().contains(muestra));
+	}
+	
+	@Test
+	void cuandoElUsuarioCambiaA5LaPreferenciaDeMuestrasARecolectarSeSeteaElCambioEnSusPreferencias() {
+		usuario.cambiarPreferenciaDeCantidadDeMuestrasARecolectar(5);
+		int resultadoActual = usuario.getPreferenciaUsuario().getCantidadDeMestrasARecolectar();
+		int resultadoEsperado = 5;
+		assertEquals(resultadoEsperado, resultadoActual);
+	}
+	
+	@Test
+	void cuandoElUsuarioCambiaA3LaDificultadSeSeteaElCambioEnSusPreferencias() {
+		usuario.cambiarPreferenciaDeDificultadPreferida(3);
+		int resultadoActual = usuario.getPreferenciaUsuario().getDificultadPreferida();
+		int resultadoEsperado = 3;
+		assertEquals(resultadoEsperado, resultadoActual);
+	}
+	
+	@Test
+	void cuandoElUsuarioCambiaA2000LaPreferenciaDeRecompensaSeSeteaElCambioEnSusPreferencias() {
+		usuario.cambiarPreferenciaDeRecompensaPreferida(2000);
+		double resultadoActual = usuario.getPreferenciaUsuario().getRecompensaPreferida();
+		double resultadoEsperado = 2000;
+		assertEquals(resultadoEsperado, resultadoActual);
+	}
+	
+	@Test
+	void siElPorcentajeDeCompletitudGeneralEs100CompletoElDesafio() {
+		desafio.setCantidadObjetivoDeMuestras(1);
+		usuario.aceptarDesafioDeUsuario(desafioDeUsuario);
+		usuario.recolectarMuestra(muestra, proyecto);
+		boolean usuarioCompletoDesafio = usuario.completoDesafio(desafioDeUsuario);
+		assertTrue(usuarioCompletoDesafio);
+	}
+	
+	@Test
+	void siElUsuarioRecolectoLaMitadDeLasMuestrasDelDesafioElPorcentajeDeCompletitudEs50() {
+		desafio.setCantidadObjetivoDeMuestras(2);
+		usuario.aceptarDesafioDeUsuario(desafioDeUsuario);
+		usuario.recolectarMuestra(muestra, proyecto);
+		int resultadoActual = desafioDeUsuario.porcentajeDeCompletitud();
+		int resultadoEsperado = 50;
+		assertEquals(resultadoEsperado, resultadoActual);
+	}
+	
+	@Test
+	void siElUsuarioRecolectoLaMitadDeLasMuestrasDeDosDesafiosElPorcentajeDeCompletitudGeneralEs50() {
+		desafio.setCantidadObjetivoDeMuestras(2);
+		desafio2.setCantidadObjetivoDeMuestras(2);
+		usuario.aceptarDesafioDeUsuario(desafioDeUsuario);
+		usuario.aceptarDesafioDeUsuario(desafioDeUsuario2);
+		usuario.recolectarMuestra(muestra, proyecto);
+		int resultadoActual = usuario.porcentajeDeCompletitudGeneral();
+		int resultadoEsperado = 50;
+		assertEquals(resultadoEsperado, resultadoActual);
+	}
+	
 }
