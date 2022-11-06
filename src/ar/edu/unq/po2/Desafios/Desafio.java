edbfb15edaa61f97f4aecd14978518d4b733d5be
Ivan Gomez <ivangzgomez@gmail.com>
2022-11-06 18:12:41 -0300

    constructor desafio

--- src/ar/edu/unq/po2/Desafios/Desafio.java
+++ src/ar/edu/unq/po2/Desafios/Desafio.java
@@ -18,7 +18,8 @@ public class Desafio implements ElementoDeLudificacion {
 	private double recompensa;
 	
 	
-	public Desafio(int cantidadObjetivoDeMuestras,int dificultad,RestriccionTemporal restriccionTemporal) {
+	public Desafio(AreaGeografica area,int cantidadObjetivoDeMuestras,int dificultad,RestriccionTemporal restriccionTemporal) {
+		this.area = area;
 		this.cantidadObjetivoDeMuestras = cantidadObjetivoDeMuestras;	//FS: Revisar > El Estado del desafio es de DesafioUsuario
 		this.dificultad = dificultad;
 		this.restriccionTemporal = restriccionTemporal;
